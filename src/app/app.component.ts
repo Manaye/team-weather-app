bcdf4c3daf7a7c6834ec7066c96da686a9dce582
hiwot nega <hiwotmanaye2@gmail.com>
2018-10-22 23:49:50 -0700

    ng testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
+  title = 'team-weather-app';
    applyNightBackground() {
     const currentHour = new Date().getHours();
     
